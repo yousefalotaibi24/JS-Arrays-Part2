2fccd74a940464697a5dca162bf8270da6e2cad3
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-11-02 17:37:12 +0300

    done

--- arrays2.js
+++ arrays2.js
@@ -1,1 +1,32 @@
+function isArrayLengthOdd(number) {
+  const sizeOfArray = number.length;
+  if (sizeOfArray % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+function isArrayLengthEven(number) {
+  const sizeOfArray = number.length;
+  if (sizeOfArray % 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+function addLailToArray(instructors) {
+  const newInstructors = instructors;
+  newInstructors.push("Laila");
+  return newInstructors;
+}
+function eliminateTeam(teams) {
+  const lastTeam = teams.pop();
+  return lastTeam;
+}
+function secodHalfOfArrayIfItIsEven(fruits) {
+  if (isArrayLengthEven(fruits)) {
+    return fruits.slice(2, 4);
+  }
+}
+console.log(secodHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]));
